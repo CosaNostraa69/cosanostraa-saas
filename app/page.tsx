8c8b933263769686b09a6cdbb511a54eb50642d3
CosaNostraa69 <lare.axelange.simplon@gmail.com>
2024-05-06 13:19:09 +0200

    Update page.tsx, add new section with note sorting feature

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,15 @@ import { ThemeToggle } from "./components/Themetoggle";
 
 export default function Home() {
   return (
-    <h1>Wessss</h1>);
+<section className="flex items-center justify-center bg-background h-[90vh]">
+  <div className="relative items-center w-full px-5 py-12 mx-auto lg:px-15 max-w-7xl md:px-12">
+    <div className="max-w-3xl mx-auto text-center">
+      <span className="w-auto px-6 py-3 rounded-full bg-secondary">
+        <span>Sort your notes easily.</span>
+      </span>
+
+    </div>
+  </div>
+</section>  
+  );
 }
