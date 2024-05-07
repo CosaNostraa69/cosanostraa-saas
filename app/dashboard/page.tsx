1e1fd0666560986308f2dd3aa85eac0a13cad51f
CosaNostraa69 <lare.axelange.simplon@gmail.com>
2024-05-07 17:29:14 +0200

    Refactor DashboardPage component, update layout and add notes section

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,52 @@
-export default function DashboardPage() {
-    return (
-        <div className="grid items-start gap-8">
-            <div className="fles items-center justify-between px-2">
-                <div className="grid gap-1">
-                    <h1 className="text-3xl font-bold">Your Notes</h1>
-                    <p className="text-gray-500">Here you can see and create new notes</p>
-                </div>
-            </div>
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import prisma from "../lib/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { File } from "lucide-react";
+
+async function getData(userId: string) {
+  const data = await prisma.note.findMany({
+    where: {
+      userId: userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return data; 
+}
+
+export default async function DashboardPage() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  const data = await getData(user?.id as string);
+
+  return (
+    <div className="grid items-start gap-8">
+      <div className="fles items-center justify-between px-2">
+        <div className="grid gap-1">
+          <h1 className="text-3xl font-bold">Your Notes</h1>
+          <p className="text-gray-500">Here you can see and create new notes</p>
         </div>
-    );
+        <Button asChild>
+          <Link href="/dashboard/new">Create a new Note</Link>
+        </Button>
+      </div>
+      {data && data.length < 1 ? (
+        <div className="flex min-h-[400px] flex-col items-center justify-center rounded-md border border-dashed p-8 text-center animate-in fade-in-50">
+          <div className="flex h-20 w-20 items-center justify-center rounded-full bg-primary/10">
+            <File className="w-10 h-10 text-primary" />
+          </div>
+
+          <h2 className="mt-6 text-xl font-semibold">You dont have any notes created</h2>
+          <p className="mb-8 mt-2 text-center text-sm leading-6 text-muted-foreground max-w-sm mx-auto">You currently dont have any notes, please create some so that you can see then right here.</p>
+
+          <Button asChild>
+          <Link href="/dashboard/new">Create a new Note</Link>
+        </Button>        </div>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
 }
