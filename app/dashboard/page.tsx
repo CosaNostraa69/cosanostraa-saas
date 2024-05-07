778bedded3fdb388957abf89c89795eed48d0cca
CosaNostraa69 <lare.axelange.simplon@gmail.com>
2024-05-07 14:56:29 +0200

    Refactor DashboardPage component, update layout and add notes section

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,12 @@
 export default function DashboardPage() {
     return (
-        <div>
-            <h1>Hello from the Dashboard</h1>
+        <div className="grid items-start gap-8">
+            <div className="fles items-center justify-between px-2">
+                <div className="grid gap-1">
+                    <h1 className="text-3xl font-bold">Your Notes</h1>
+                    <p className="text-gray-500">Here you can see and create new notes</p>
+                </div>
+            </div>
         </div>
     );
 }
